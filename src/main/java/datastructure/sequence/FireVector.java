52c0395fedbe590a7b32ca0f4b2430ba80a4440e
liuyi27 <liuyi27@58.com>
2020-05-28 18:22:44 +0800

    add FireVector

---
+++ src/main/java/datastructure/sequence/FireVector.java
@@ -0,0 +1,25 @@
+package datastructure.sequence;
+
+
+import java.util.Vector;
+
+public class FireVector {
+
+    public static void main(String[] args) {
+        Vector<Integer> v = new Vector<>();
+        v.add(1);
+        v.add(1,1);
+        v.addElement(1);
+        v.contains(1);
+        v.elementAt(0);
+        v.get(0);
+        v.isEmpty();
+        v.remove(1);
+        v.remove(1);
+        v.size();
+        v.set(1,1);
+        
+
+
+    }
+}
---
+++ src/main/java/keyword/FireTransient.java
@@ -0,0 +1,94 @@
+package keyword;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.*;
+
+public class FireTransient {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        serializeUser1();
+        deSerializeUser1();
+    }
+
+    private static void serializeUser1() throws IOException {
+        File file = new File("/opt/test/");
+        if (!file.exists()){
+            file.mkdirs();
+        }
+        User1 u = new User1();
+        u.setAge(18);
+        u.setName("fire");
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("/opt/test/template1"));
+        oos.writeObject(u);
+        oos.close();
+    }
+
+    private static void deSerializeUser1() throws IOException, ClassNotFoundException {
+
+        File file = new File("/opt/test/template1");
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        User1 u = (User1) ois.readObject();
+        System.out.println(u.toString());
+    }
+
+    private static void serializeUser() throws IOException {
+        File file = new File("/opt/test/");
+        if (!file.exists()){
+            file.mkdirs();
+        }
+        User u = new User();
+        u.setAge(10);
+        u.setName("fire");
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("/opt/test/template"));
+        oos.writeObject(u);
+        oos.close();
+    }
+
+    private static void deSerializeUser() throws IOException, ClassNotFoundException {
+
+        File file = new File("/opt/test/template");
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        User u = (User) ois.readObject();
+        System.out.println(u.toString());
+    }
+}
+
+
+
+@Getter
+@Setter
+@ToString
+class User1 implements Externalizable{
+    private transient int age;
+    private String name;
+
+     //由于实现了Externalizable接口的类，会调用构造函数，
+     // 而User1的构造函数是私有的。无法訪问，从而导致抛出异常。
+    public User1(){
+
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(age);
+        out.writeObject(name);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        age = (int)in.readObject();
+        name = (String) in.readObject();
+    }
+}
+
+
+@Getter
+@Setter
+@ToString
+class User implements Serializable{
+    private transient int age;
+    private String name;
+}
